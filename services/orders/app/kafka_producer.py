ba32931c14a2e12dc6a5a232d5c1fef5554dd567
aiwesty <dmitriistrekalin@gmail.com>
2026-01-19 17:11:05 +0300

    Добавлен kafka producer для orders

---
+++ services/orders/app/kafka_producer.py
@@ -0,0 +1,37 @@
+import json 
+import logging
+from aiokafka import AIOKafkaProducer
+from app.config import settings
+
+
+logger = logging.getLogger(__name__)
+
+class KafkaProducerClient: 
+    """Синглтон для управления соединением с кафкой на стороне producer'a, так просто проще чем такщить DI"""
+    def __init__(self) -> None:
+        self.producer = None #задаем producer как None для переопределения
+        
+    async def start(self): 
+        self.producer = AIOKafkaProducer(
+            bootstrap_servers=settings.kafka_bootstrap_servers, #прокидываем брокера, для подключения и получения данных о остальных брокерах
+            value_serializer=lambda v: json.dumps(v).encode("utf-8")#переводим данные в bytes понятные для kafka
+        )
+        await self.producer.start() #запускаем producer
+        logger.info("Kafka Producer started")    
+        
+    async def stop(self): 
+        if self.producer: #проверяем есть ли инстанс, чтобы закрыть его 
+            await self.producer.stop()
+            
+    async def send_message(self, topic: str, message: dict): #принимаем куда и что(в каком виде пишем)
+        if not self.producer: #есть ли инстанс кафки
+            raise RuntimeError("Kafka Producer is not started")
+        
+        try:
+            await self.producer.send_and_wait(topic, message)#пытаемся отправить сообщение в топик с подтверждением того что оно пришло
+        except Exception as e:
+            logger.error(f"Failed to send message to {topic}: ", e)
+            # вреальном проде тут нужен ретрай или outbox pattern
+            raise # прокидываем выше
+        
+        
